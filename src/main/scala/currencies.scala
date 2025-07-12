4fb7f115a50a643addcc37469c09dc1780e68423
Hubert1225 <hubibaran@gmail.com>
2025-07-12 23:10:23 +0200

    add Scalafmt plugin for code formatting, reformat code

--- src/main/scala/currencies.scala
+++ src/main/scala/currencies.scala
@@ -10,50 +10,58 @@ import utils._
 type CurrencyCode = String
 
 case class CurrenciesData(
-  baseCurrency: CurrencyCode,
-  rates: Map[CurrencyCode, Double],
-  timeMoment: Instant,
+    baseCurrency: CurrencyCode,
+    rates: Map[CurrencyCode, Double],
+    timeMoment: Instant
 )
 
 class CurrenciesDataRepository extends JsonApiHandler:
   private lazy val currenciesDataEndpoint = "https://open.er-api.com/v6/latest/USD"
 
-  private def validateCurrencyDataMessage(message: ujson.Value.Value): Try[LinkedHashMap[String, ujson.Value.Value]] =
+  private def validateCurrencyDataMessage(
+      message: ujson.Value.Value
+  ): Try[LinkedHashMap[String, ujson.Value.Value]] =
     val messageObjTry = Try(message.obj)
     messageObjTry match
       case Success(messageObj) =>
         if messageObj.contains("result") && messageObj("result").str == "success"
         then Success(messageObj)
-        else Failure(
-          new Exception("The field \"result\" not present in the currencies data JSON or its value is not \"success\"")
-        )
+        else
+          Failure(
+            new Exception(
+              "The field \"result\" not present in the currencies data JSON or its value is not \"success\""
+            )
+          )
       case Failure(exception) => messageObjTry
 
-  private def currenciesDataToObject(currenciesData: LinkedHashMap[String, ujson.Value.Value]): Try[CurrenciesData] = Try(
+  private def currenciesDataToObject(
+      currenciesData: LinkedHashMap[String, ujson.Value.Value]
+  ): Try[CurrenciesData] = Try(
     CurrenciesData(
       baseCurrency = currenciesData("base_code").str,
-      rates = (for (curCode, curRate) <- currenciesData("rates").obj.toMap yield (curCode, curRate.num)),
+      rates =
+        (for (curCode, curRate) <- currenciesData("rates").obj.toMap yield (curCode, curRate.num)),
       timeMoment = Instant.ofEpochSecond(currenciesData("time_last_update_unix").num.toLong)
     )
   )
 
   def fetch(): Try[CurrenciesData] =
     request_get(currenciesDataEndpoint)
-    .flatMap(validateCurrencyDataMessage)
-    .flatMap(currenciesDataToObject)
-  
+      .flatMap(validateCurrencyDataMessage)
+      .flatMap(currenciesDataToObject)
 
-def convertToBaseCurrency(
-  value: Double, currentCurrency: CurrencyCode)(using currenciesData: CurrenciesData): Double =
+def convertToBaseCurrency(value: Double, currentCurrency: CurrencyCode)(using
+    currenciesData: CurrenciesData
+): Double =
   value / currenciesData.rates(currentCurrency)
 
+def convertFromBaseCurrency(value: Double, targetCurrency: CurrencyCode)(using
+    currenciesData: CurrenciesData
+): Double =
+  value * currenciesData.rates(targetCurrency)
 
-def convertFromBaseCurrency(
-  value: Double, targetCurrency: CurrencyCode)(using currenciesData: CurrenciesData): Double =
-    value * currenciesData.rates(targetCurrency)
-
-
-def convertCurrency(
-  value: Double, currentCurrency: CurrencyCode, targetCurrency: CurrencyCode)(using currenciesData: CurrenciesData): Double =
-    val valueInBaseCurrency = convertToBaseCurrency(value, currentCurrency)
-    convertFromBaseCurrency(valueInBaseCurrency, targetCurrency)
+def convertCurrency(value: Double, currentCurrency: CurrencyCode, targetCurrency: CurrencyCode)(
+    using currenciesData: CurrenciesData
+): Double =
+  val valueInBaseCurrency = convertToBaseCurrency(value, currentCurrency)
+  convertFromBaseCurrency(valueInBaseCurrency, targetCurrency)
--- src/main/scala/utils.scala
+++ src/main/scala/utils.scala
@@ -6,14 +6,12 @@ import sttp.client4.Response
 import ujson.read
 import ujson.Value.Value
 
-/**
- * A utility for convenient interaction with Web APIs sending
- * JSON data 
- */
+/** A utility for convenient interaction with Web APIs sending JSON data
+  */
 trait JsonApiHandler:
   private def tryParseJson(body: String): Try[ujson.Value.Value] =
     Try(ujson.read(body))
 
   def request_get(url: String): Try[ujson.Value.Value] =
     Try(quickRequest.get(uri"$url").send())
-    .flatMap((response: Response[String]) => tryParseJson(response.body))
+      .flatMap((response: Response[String]) => tryParseJson(response.body))
