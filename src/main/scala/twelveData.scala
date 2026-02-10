dad2e67b51b265e85aeff9333010f470a3268f07
Hubert Baran <hubert.baran1225@gmail.com>
2026-02-10 23:13:10 +0100

    reformat, add 'getStocks' fun

--- src/main/scala/twelveData.scala
+++ src/main/scala/twelveData.scala
@@ -3,8 +3,11 @@ package twelveData
 import scala.util.{Try, Success, Failure}
 import ujson.Value.Value
 import upickle.core.LinkedHashMap
+import sttp.client4.*
+import sttp.model.*
 
 import utils._
+import currencies.CurrencyCode
 
 case class Exchange(
     title: String,
@@ -14,16 +17,33 @@ case class Exchange(
     timezone: String
 )
 
+case class Stock(
+    symbol: String,
+    name: String,
+    currency: CurrencyCode,
+    exchange: String,
+    micCode: String,
+    country: String,
+    figiCode: String
+)
+
 class TwelveDataClient extends JsonApiHandler:
 
   private val apiKey = sys.env("TWELVE_DATA_API_KEY")
+  private val endpointBase = "https://api.twelvedata.com/"
+  private val defaultParams = Map("apikey" -> apiKey)
 
-  lazy val endpointBase = "https://api.twelvedata.com/"
-
-  private def endpointUrl(endpoint: String): String =
-    endpointBase.concat(endpoint).concat(s"?apikey=$apiKey")
+  private def endpointUrl(endpoint: String)(
+      queryParams: Option[Map[String, String]] = None
+  ): String =
+    val resolvedParams = queryParams match
+      case Some(params) => defaultParams ++ params
+      case None         => defaultParams
+    val endpointUri = uri"$endpointBase/$endpoint?$resolvedParams"
+    endpointUri.toString
 
   lazy val exchangesEndpoint = endpointUrl("exchanges")
+  lazy val stocksEndpoint = endpointUrl("stocks")
 
   private def validateResponse(
       responseObj: LinkedHashMap[String, ujson.Value.Value]
@@ -35,7 +55,9 @@ class TwelveDataClient extends JsonApiHandler:
         new Exception("Response does not contain the \"status\" or status is unsuccessful")
       )
 
-  def parseExchangeObject(exchangeJsonObj: LinkedHashMap[String, ujson.Value.Value]): Exchange =
+  private def parseExchangeObject(
+      exchangeJsonObj: LinkedHashMap[String, ujson.Value.Value]
+  ): Exchange =
     Exchange(
       title = exchangeJsonObj("title").str,
       name = exchangeJsonObj("name").str,
@@ -44,9 +66,27 @@ class TwelveDataClient extends JsonApiHandler:
       timezone = exchangeJsonObj("timezone").str
     )
 
-  def getExchanges(): Try[List[Exchange]] =
-    requestGet(exchangesEndpoint)
-      .map(_.obj)
-      .flatMap(validateResponse)
-      .map(_("data").arr.toList)
-      .map(_.map((exchangeJsonObj: ujson.Value.Value) => parseExchangeObject(exchangeJsonObj.obj)))
+  private def parseStockObject(stockJsonObj: LinkedHashMap[String, ujson.Value.Value]): Stock =
+    Stock(
+      symbol = stockJsonObj("symbol").str,
+      name = stockJsonObj("name").str,
+      currency = stockJsonObj("currency").str,
+      exchange = stockJsonObj("exchange").str,
+      micCode = stockJsonObj("mic_code").str,
+      country = stockJsonObj("country").str,
+      figiCode = stockJsonObj("figi_code").str
+    )
+
+  private def getObjectsFun[T](
+      parseFun: (LinkedHashMap[String, ujson.Value.Value]) => T,
+      endpoint: Option[Map[String, String]] => String
+  ) =
+    (queryParams: Option[Map[String, String]]) =>
+      requestGet(endpoint(queryParams))
+        .map(_.obj)
+        .flatMap(validateResponse)
+        .map(_("data").arr.toList)
+        .map(_.map((jsonObj: ujson.Value.Value) => parseFun(jsonObj.obj)))
+
+  val getExchanges = getObjectsFun(parseExchangeObject, exchangesEndpoint)
+  val getStocks = getObjectsFun(parseStockObject, stocksEndpoint)
